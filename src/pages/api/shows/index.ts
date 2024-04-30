ac953d4fb172ec2481e5f117472ec5b7513c857e
madaley1 <mor2daley@gmail.com>
2024-04-30 14:20:39 -0700

    fix prettier & lint

--- src/pages/api/shows/index.ts
+++ src/pages/api/shows/index.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
-const getAllshows = async () => {
+const getAllShows = async () => {
   return await prisma.shows.findMany();
-}
+};
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
-  const shows = await getAllshows();
-  res.status(200).json(shows)
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const shows = await getAllShows();
+  res.status(200).json(shows);
 }
