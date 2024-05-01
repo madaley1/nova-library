a8fd11834ada4985c0ae17ec830dce7180165b63
madaley1 <mor2daley@gmail.com>
2024-04-30 19:58:36 -0700

    WIP: resource pages

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,7 +1,42 @@
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { log } from 'console';
 import { useRouter } from 'next/router';
-export default function Index() {
-  const router = useRouter();
+import { useState } from 'react';
 
-  console.log(router.query.id);
-  return <div>test</div>;
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  console.log(id);
+  const response = await fetch(`${process.env.URL}/api/books`);
+  const data = await response.json();
+  return {
+    props: { data },
+  };
+}
+
+export default function Index({ data }) {
+  const [resourceData, setResourceData] = useState(data);
+  // const router = useRouter();
+  // const { id } = router.query;
+  console.log(data);
+
+  return (
+    // <></>
+    <DataGrid
+      columns={
+        data[0] ? Object.keys(data[0]).map((key) => ({ field: key, headerName: key, hideable: key === 'id' })) : []
+      }
+      rows={data}
+      slots={{
+        toolbar: GridToolbar,
+      }}
+      checkboxSelection
+      initialState={{
+        columns: {
+          columnVisibilityModel: {
+            id: false,
+          },
+        },
+      }}
+    />
+  );
 }
