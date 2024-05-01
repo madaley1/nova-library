bf8abe255a1b4db9fb42af839065cdf716dec3b3
madaley1 <mor2daley@gmail.com>
2024-04-30 19:58:36 -0700

    WIP: resource pages

--- src/pages/api/books/index.ts
+++ src/pages/api/books/index.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
 const getAllBooks = async () => {
   return await prisma.books.findMany();
-}
+};
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const books = await getAllBooks();
-  res.status(200).json(books)
+  res.status(200).json(books);
 }
