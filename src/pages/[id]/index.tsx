03dc053186cb30f3cd9b186b89d4e225f0eedde5
madaley1 <mor2daley@gmail.com>
2024-05-08 11:04:49 -0700

    WIP - add modals

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,6 +1,10 @@
-import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { EditRowModal } from '@/components/libraryComponents/EditRow';
+import { Button, Grid } from '@mui/material';
+import { DataGrid, GridCellParams, GridColDef, GridToolbar } from '@mui/x-data-grid';
+import { NextPageContext } from 'next';
+import React, { useState } from 'react';
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: NextPageContext) {
   const { id } = context.query;
   const response = await fetch(`${process.env.URL}/api/${id}`);
   const data = await response.json();
@@ -9,24 +13,64 @@ export async function getServerSideProps(context) {
   };
 }
 
-export default function Index({ data }) {
+export default function Index({ data }: Record<string, any>) {
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const openEditModal = () => {
+    setEditModalOpen(true);
+  };
+  const closeEditModal = () => {
+    setEditModalOpen(false);
+  };
+
+  const columns: GridColDef[] =
+    data[0] ? Object.keys(data[0]).map((key) => ({ field: key, headerName: key, hideable: key === 'id' })) : [];
+  columns.push({
+    field: 'edit',
+    headerName: 'edit',
+    hideable: false,
+    renderCell: (params: GridCellParams) => {
+      const onClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        console.log(e, params);
+        openEditModal();
+      };
+      return (
+        <>
+          <Button onClick={onClick}>Edit</Button>
+        </>
+      );
+    },
+  });
+
   return (
-    <DataGrid
-      columns={
-        data[0] ? Object.keys(data[0]).map((key) => ({ field: key, headerName: key, hideable: key === 'id' })) : []
-      }
-      rows={data}
-      slots={{
-        toolbar: GridToolbar,
-      }}
-      checkboxSelection
-      initialState={{
-        columns: {
-          columnVisibilityModel: {
-            id: false,
+    <>
+      <DataGrid
+        columns={columns}
+        rows={data}
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        checkboxSelection
+        initialState={{
+          columns: {
+            columnVisibilityModel: {
+              id: false,
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+      <Grid container sx={{ display: 'flex', flexDirection: 'row wrap', marginTop: '1em' }}>
+        <Grid item>
+          <Button>Add New Item</Button>
+        </Grid>
+        <Grid item>
+          <Button>Delete Selected</Button>
+        </Grid>
+        <Grid item>
+          <Button>Delete All</Button>
+        </Grid>
+      </Grid>
+      <EditRowModal open={editModalOpen} closeModal={closeEditModal} />
+    </>
   );
 }
