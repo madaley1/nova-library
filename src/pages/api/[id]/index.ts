523247bf1a3576be94f61d244863b2faa9c57545
madaley1 <mor2daley@gmail.com>
2024-05-13 10:51:06 -0700

    WIP: api requests generic - need to fix the type error on findMany

--- src/pages/api/[id]/index.ts
+++ src/pages/api/[id]/index.ts
@@ -4,28 +4,25 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const prisma = new PrismaClient();
 
 const getAllOfResource = async (resourceName: Prisma.ModelName) => {
-  const resource = prisma[resourceName as Prisma.ModelName];
-  if (!resource.findMany) throw Error('Error: findMany not found, does resource exist?');
-  return await resource.findMany();
+  const resource = prisma[resourceName];
+  if (resource.findMany) {
+    return await resource.findMany();
+  } else {
+    throw Error('Error: findMany not found, does resource exist?');
+  }
 };
 
-const validateResourceTypeExists = () => {};
+const validateResourceTypeExists = (resourceType: unknown): resourceType is Prisma.ModelName => {
+  return typeof resourceType === 'string' && Object.keys(Prisma.ModelName).includes(resourceType);
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const requestedResource = req.query.id;
-  console.log(req.query);
-  if (Array.isArray(requestedResource)) {
-    return res.status(500).json({
-      message: 'Please provide a string when requesting resources',
-    });
-  } else if (!requestedResource) {
-    return res.status(500).json({
-      message: 'Please provide a string when requesting resources',
+  if (!validateResourceTypeExists(requestedResource)) {
+    res.status(500).json({
+      message: 'Please use a valid resource type',
     });
   } else {
-    if (!(requestedResource instanceof prisma.ModelName)) {
-      return res.status(500).json({ message: 'Please provide a valid resource name' });
-    }
     const resource = await getAllOfResource(requestedResource);
     return res.status(200).json(resource);
   }
