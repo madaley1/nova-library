4615ef010863e70050e4b4b43af1b1b2cca204b5
madaley1 <mor2daley@gmail.com>
2024-05-23 09:34:04 -0700

    fetch library templates and add to redux

---
+++ src/resources/libraryData.ts
@@ -0,0 +1,16 @@
+import { libraryTemplate } from '@/utils/libraries/templates';
+import { createSlice } from '@reduxjs/toolkit';
+
+const resourceData = createSlice({
+  name: 'Resource Data',
+  initialState: {
+    templates: Array<libraryTemplate>()
+  },
+  reducers: {
+    setTemplates: (state, action) => ({ ...state, templates: action.payload }),
+    addTemplate:  (state, action) => ({ ...state, templates: [...state.templates, action.payload] }),
+  },
+});
+
+export const { setTemplates } = resourceData.actions;
+export default resourceData.reducer;
