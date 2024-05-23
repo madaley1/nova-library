00ceb8b3e4bb66bd2816068959a9ff8b9c329035
madaley1 <mor2daley@gmail.com>
2024-05-23 08:52:24 -0700

    update db creation to include templates table

---
+++ sql/templates.sql
@@ -0,0 +1,25 @@
+CREATE TABLE template_store (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(25),
+    fields varchar(15000)
+);
+INSERT INTO template_store (title, fields) VALUES (
+	'books',
+    '{"id":"number","title":"string","releaseDate":"date","genre":"multiSelect","isbn":"string"}'
+);
+INSERT INTO template_store (title, fields) VALUES (
+	'movies',
+    '{"id":"number","title":"string","releaseDate":"date","genre":"multiSelect","rating":"string"}'
+);
+INSERT INTO template_store (title, fields) VALUES (
+	'tv shows',
+    '{"id":"number","title":"string","releaseDate":"date","genre":"multiSelect","rating":"string"}'
+);
+INSERT INTO template_store (title, fields) VALUES (
+	'video games',
+    '{"id":"number","title":"string","releaseDate":"date","genre":"multiSelect","rating":"string"}'
+);
+INSERT INTO template_store (title, fields) VALUES (
+	'custom',
+    '{"id":"number","title":"string","releaseDate":"date","genre":"multiSelect"}'
+);
