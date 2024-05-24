37f4f08b7429afdbf70aee8abc54893fe1251d17
madaley1 <mor2daley@gmail.com>
2024-05-24 14:02:03 -0700

    WIP add new library modal - finish up the edit fields screen - still need to add the option to add new fields - I want to finish up the submission portion first though.

--- src/components/libraryComponents/AddNewLibrary/StepTwo.tsx
+++ src/components/libraryComponents/AddNewLibrary/StepTwo.tsx
@@ -1,5 +1,4 @@
-import { fieldTypes } from '@/utils/libraries/templates';
-import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
+import { FieldType, fieldTypes } from '@/utils/libraries/templates';
 import {
   Box,
   Button,
@@ -13,7 +12,7 @@ import {
   TextField,
 } from '@mui/material';
 import { DateTimePicker } from '@mui/x-date-pickers';
-import { AddNewLibraryContext } from './AddNewLibraryContext';
+import { AddNewLibraryContext, useLibraryData } from './AddNewLibraryContext';
 
 type StepTwoProps = {
   continueFunction: () => void;
@@ -22,59 +21,87 @@ type StepTwoProps = {
 };
 
 const StepTwo = (props: StepTwoProps) => {
+  const [libraryState, setData] = useLibraryData();
   return (
     <AddNewLibraryContext.Consumer>
       {(value) => {
         if (value === null) return <>Something has gone wrong, please reload and try again</>;
         const [state, dispatch] = value;
+        let newState = state;
+
         return (
           <Container sx={{ display: `${props.active ? 'flex' : 'none'}`, flexFlow: 'column nowrap' }}>
-            <FormControl>
-              {Object.entries(state.fields)
-                .filter((entry) => entry[0] !== 'id')
-                .map((field) => {
-                  const [title, type] = field;
-                  // const InputType = ({ type }: { type: fieldTypes }) => {
-                  //   if (type === 'string') return <TextField type={type}></TextField>;
-                  //   else if (type === 'number') return <NumberInput />;
-                  //   else if (type === 'select' || type === 'multiSelect') return <Select></Select>;
-                  //   else if (type === 'date') return <DateTimePicker />;
-                  // };
-                  // return (
-                  //   <Box key={title} sx={{ display: 'flex', flexFlow: 'column', marginBottom: '1em' }}>
-                  //     <FormLabel sx={{ textTransform: 'capitalize' }}>{title}</FormLabel>
-                  //     <InputType type={type} />
-                  //   </Box>
-                  // );
-                  return (
-                    <Box key="title" sx={{ display: 'flex', flexFlow: 'row nowrap' }}>
-                      <Box
-                        sx={{
-                          display: 'flex',
-                          flexFlow: 'column nowrap',
-                          marginRight: '1em',
-                          width: '35em',
-                          height: '5em',
+            {Object.entries(state.fields)
+              .filter((entry) => entry[0] !== 'id')
+              .map((field, index) => {
+                const [title, type] = field;
+                // const InputType = ({ type }: { type: fieldTypes }) => {
+                //   if (type === 'string') return <TextField type={type}></TextField>;
+                //   else if (type === 'number') return <NumberInput />;
+                //   else if (type === 'select' || type === 'multiSelect') return <Select></Select>;
+                //   else if (type === 'date') return <DateTimePicker />;
+                // };
+                // return (
+                //   <Box key={title} sx={{ display: 'flex', flexFlow: 'column', marginBottom: '1em' }}>
+                //     <FormLabel sx={{ textTransform: 'capitalize' }}>{title}</FormLabel>
+                //     <InputType type={type} />
+                //   </Box>
+                // );
+                return (
+                  <Box key={index} sx={{ display: 'flex', flexFlow: 'row nowrap' }}>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexFlow: 'column nowrap',
+                        marginRight: '1em',
+                        width: '35em',
+                        height: '5em',
+                      }}
+                    >
+                      <TextField
+                        label="Field Name"
+                        defaultValue={title}
+                        onChange={(e) => {
+                          const fieldEntries = Object.entries(newState.fields).map(([key, value], entry) => {
+                            if (entry === index + 1) return [e.target.value, value];
+                            else return [key, value];
+                          });
+                          newState = { ...newState, fields: Object.fromEntries(fieldEntries) };
+                        }}
+                      ></TextField>
+                    </Box>
+                    <Box sx={{ display: 'flex', flexFlow: 'column nowrap', height: '5em', width: '15em' }}>
+                      <Select
+                        label="Field Type"
+                        defaultValue={type}
+                        onChange={(e) => {
+                          const valueIsFieldType = (value: unknown): value is FieldType => {
+                            return fieldTypes.includes(value as FieldType);
+                          };
+                          if (valueIsFieldType(e.target.value))
+                            newState = { ...newState, fields: { ...newState.fields, [title]: e.target.value } };
                         }}
                       >
-                        <TextField label="Field Name" value={title}></TextField>
-                      </Box>
-                      <Box sx={{ display: 'flex', flexFlow: 'column nowrap', height: '5em', width: '15em' }}>
-                        <Select label="Field Type" value={type}>
-                          {fieldTypes.map((type) => (
-                            <MenuItem value={type} key={type} sx={{ width: '100%', textTransform: 'capitalize' }}>
-                              {type}
-                            </MenuItem>
-                          ))}
-                        </Select>
-                      </Box>
+                        {fieldTypes.map((type) => (
+                          <MenuItem value={type} key={title + type} sx={{ width: '100%', textTransform: 'capitalize' }}>
+                            {type}
+                          </MenuItem>
+                        ))}
+                      </Select>
                     </Box>
-                  );
-                })}
-            </FormControl>
+                  </Box>
+                );
+              })}
             <ButtonGroup variant="outlined">
               <Button onClick={props.goBackFunction}>Back</Button>
-              <Button onClick={props.continueFunction}>Continue</Button>
+              <Button
+                onClick={() => {
+                  setData(newState);
+                  props.continueFunction();
+                }}
+              >
+                Continue
+              </Button>
             </ButtonGroup>
           </Container>
         );
