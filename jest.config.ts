aa40577470f1b475be807e8759af690b60fea41e
madaley1 <mor2daley@gmail.com>
2024-05-30 15:26:10 -0700

    add mysql test config

--- jest.config.ts
+++ jest.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -26,7 +26,7 @@ const config: Config = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -37,18 +37,13 @@ const config: Config = {
   // ],
 
   // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "mjs",
-    "cjs",
-    "jsx",
-    "ts",
-    "tsx",
-    "json",
-    "node"
-  ],
+  moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
+
+  // jest test setup
+  globalSetup: 'node_modules/@databases/mysql-test/jest/globalSetup',
+  globalTeardown: '/node_modules/@databases/mysql-test/jest/globalTeardown',
 };
 const createJestConfig = nextJest({
-  dir: './'
-})
+  dir: './',
+});
 export default createJestConfig(config);
