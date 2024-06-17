5da8dff293ffd848e4e4a7f7a18e678d41793c88
madaley1 <mor2daley@gmail.com>
2024-06-17 10:15:45 -0700

    fix connection endings

--- src/utils/mysqlConection.ts
+++ src/utils/mysqlConection.ts
@@ -8,10 +8,17 @@ export const connection = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 });
 
+export const testConnection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: parseInt(`${process.env.MYSQL_EXTERNAL_PORT || 3306}`),
+  database: 'testDB',
+});
+
 export function runMySqlQuery(query: string, connection: Pool) {
   return new Promise<Record<string, any>>((res, rej) => {
     connection.query(query, (error, results, fields) => {
-      connection.end();
       if (error) rej(error);
       res({ results, fields });
     });
