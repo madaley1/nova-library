622a60f2dec8c3e7bdf42549dd24c06f3aed2e8f
madaley1 <mor2daley@gmail.com>
2024-06-25 11:30:45 -0700

    update test labels to denote test type better - In particular, removing spec from non spec files - Using .test.ts(x) for component tests - Using .inte.ts for integration tests

--- src/pages/api/nav/tests/nav.inte.ts
+++ src/pages/api/nav/tests/nav.inte.ts
@@ -1,7 +1,7 @@
 import { runMySqlQuery } from '@/utils/mysqlConection';
 import { testConnection } from '@/utils/tests/jestUtils';
 import { describe, expect, it } from '@jest/globals';
-import { processResults, sql_getAllTables } from '../';
+import { processResults, sql_getAllTables } from '..';
 
 describe('nav api', () => {
   it('nav query should work', async () => {
