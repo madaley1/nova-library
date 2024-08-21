8e48127f0e21bb1348910793a80398c23e3d24aa
madaley1 <mor2daley@gmail.com>
2024-08-21 11:32:59 -0700

    backend feat: delete items from db

--- backend/routes/libraries/items.py
+++ backend/routes/libraries/items.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from utils.items.add_new_items import add_new_items
+from utils.items.delete_items_from_library import delete_items_from_library
 
 router = APIRouter()
 
@@ -10,9 +11,10 @@ def add_items_to_library(library_title: str, items: list[dict[str,str | int | fl
   return add_items
 
 @router.put('/libraries/{library}/{item_id}')
-def edit_item_in_library(library: str, item_id: str):
+def edit_item_in_library(library_title: str, item_id: str):
   return {"path": "to edit a specific item in a library"}
 
 @router.delete('/libraries/{library}/items')
-def delete_item_in_library(library: str, item_ids: list[str]):
-  return {"path": "to delete items in a library"}
+def delete_item_in_library(library_title: str, item_ids: list[str]):
+  delete_items = delete_items_from_library(library_title=library_title, item_ids=item_ids)
+  return delete_items
--- backend/utils/items/delete_items_from_library.py
+++ backend/utils/items/delete_items_from_library.py
@@ -3,3 +3,18 @@ from utils.connection import engine
 
 def delete_items_from_library(library_title: str, item_ids: list[str]):
   print('delete')
+  id_str = '('
+  for item_id in item_ids:
+    id_str += '({item_id}),'.format(item_id=item_id)
+  id_str = id_str.strip(',')
+  id_str += ')'
+  delete_items_query = 'DELETE FROM {library_title}_data WHERE ID IN {id_str};'.format(library_title=library_title, id_str=id_str)
+  print(delete_items_query)
+  with engine.connect() as connection:
+    connection.execute(text(delete_items_query))
+    connection.commit()
+    connection.close()
+  return {"message": "Items {item_ids} have been deleted".format(item_ids=item_ids)}
+
+
+delete_items_from_library(library_title='test', item_ids=[3])
