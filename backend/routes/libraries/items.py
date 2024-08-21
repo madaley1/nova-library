f0e8bb23a7b8cc160bf0ea70c1999f56044b2c1d
madaley1 <mor2daley@gmail.com>
2024-08-21 11:44:15 -0700

    backend fix: fix path variable mismatch:

--- backend/routes/libraries/items.py
+++ backend/routes/libraries/items.py
@@ -10,11 +10,11 @@ def add_items_to_library(library_title: str, items: list[dict[str,str | int | fl
   add_items = add_new_items(library_title=library_title, items=items)
   return add_items
 
-@router.put('/libraries/{library}/{item_id}')
+@router.put('/libraries/{library_title}/{item_id}')
 def edit_item_in_library(library_title: str, item_id: str):
   return {"path": "to edit a specific item in a library"}
 
-@router.delete('/libraries/{library}/items')
+@router.delete('/libraries/{library_title}/items')
 def delete_item_in_library(library_title: str, item_ids: list[str]):
   delete_items = delete_items_from_library(library_title=library_title, item_ids=item_ids)
   return delete_items
